4606026e2563fa710f4f4e9fddc580a30ff3a5dc
Sergey Gilmanov <gilmanov10@yandex.ru>
2018-11-08 23:47:08 +0300

    lesson 5 and 6(not full)

--- lesson5/secon_page.php
+++ lesson5/secon_page.php
@@ -20,32 +20,4 @@ mysqli_query($connection, $update) or die(mysqli_error($connection));
 <h4>Просмотров <?= $image['count'] ?></h4>
 
 
-<?php
-    $fbQuery = "SELECT * FROM feedback WHERE product_id = {$id}";
-    foreach ($fbQuery as $feedback) {
-        if ($feedback) {
-            echo "<br><b>Пользователь: </b>" . $feedback["user_name"];
-            echo "<br><b>Текст: </b>" . $feedback["text"];
-        } else {
-            echo "<br>Отзыв отсутствует! ";
-        }
-    }
-    echo "<br><a href='view.php?id={$id}&addcom=form'>Добавить отзыв</a>" . "<hr>";
 
-
-    $name = $_GET['name'];
-    $text = $_GET['text'];
-    executeQuery("INSERT INTO feedback (user_name, text, product_id) 
-    VALUES('{$name}','{$text}', '{$id}')");
-
-
-
-    echo "
-    <form action='' method='get'>
-    <input name='id' value='$id' type='hidden'/>
-    <input name='addcom' value='view' type='hidden'/>
-    <b>Пользователь:</b> <input name='name' type='text' required/><br>
-    <b>Текст:</b><br><textarea name='text' id='' cols='30' rows='10' required></textarea><br><br>
-    <input value='Отправить' type='submit'/>
-    </form>
-    ";
