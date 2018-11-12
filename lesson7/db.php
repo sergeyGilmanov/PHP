3e0602ee1b9521155e35dbd33411c6a896c9be3c
Sergey Gilmanov <gilmanov10@yandex.ru>
2018-11-13 02:12:47 +0300

    add lesson7(start)

--- lesson7/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$connection  = mysqli_connect('localhost', 'root', '', test_db);
-if ($connection == false){
-    echo 'Ошибка соединения с базой данных: ' . mysqli_connect_error();
-    exit;}
